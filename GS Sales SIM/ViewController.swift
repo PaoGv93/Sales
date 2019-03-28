d00206e5bbc50d2e8834b437adc80a1d14ae300e
Paola Garzón <gorda@Ricardos-MacBook-Pro.local>
2019-03-28 15:15:06 -0400

    first commit

---
+++ GS Sales SIM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GS Sales SIM
+//
+//  Created by Paola Garzón on 3/28/19.
+//  Copyright © 2019 Paola Garzón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
