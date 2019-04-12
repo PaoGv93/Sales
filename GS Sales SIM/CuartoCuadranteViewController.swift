f8d18181134f4a2fac07c4db4211adb99f0859eb
Paola Garzón <gorda@Ricardos-MacBook-Pro.local>
2019-04-12 10:34:38 -0400

    añadi todas las graficas y recomendaciones, quita picker y agrega label de rfc

--- GS Sales SIM/CuartoCuadranteViewController.swift
+++ GS Sales SIM/CuartoCuadranteViewController.swift
@@ -7,24 +7,93 @@
 //
 
 import UIKit
+import Charts
 
 class CuartoCuadranteViewController: UIViewController {
 
+    @IBOutlet weak var radarChartView: RadarChartView!
+    @IBOutlet weak var barChart: BarChartView!
+    
+    //los valores son los mismos parar las dos graficas
+    let months = ["January", "Febrary", "March", "April", "May"]
+    let unitsSold = [30.0, 50.0, 100.0, 75.0, 85.0]
+    
+    weak var axisFormatDelegate: IAxisValueFormatter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setRadarChart(dataPoints: months, values: unitsSold)
+        let  xAxis : XAxis = self.radarChartView.xAxis
+        xAxis.labelFont = UIFont(name: "Arial-BoldMT", size: 14.0)!
+        let  yAxis : YAxis = self.radarChartView.yAxis
+        yAxis.labelFont = UIFont(name: "Arial-BoldMT", size: 10.0)!
+        
+        
+        axisFormatDelegate = self as? IAxisValueFormatter
+        setBarChart(dataEntryX: months, dataEntryY: unitsSold)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //Funcion de la radarChart
+    func setRadarChart(dataPoints: [String], values: [Double]) {
+        radarChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [ChartDataEntry] = []
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        let chartDataSet = RadarChartDataSet(values: dataEntries, label: "Units Sold")
+        
+        //Options of radarChart
+        radarChartView.sizeToFit()
+        radarChartView.chartDescription?.text = ""
+        
+        //Options for the axis from here. The range is 0-100, the interval is 10
+        radarChartView.yAxis.forceLabelsEnabled = true
+        radarChartView.yAxis.labelCount = 10
+        radarChartView.yAxis.axisMaximum = 80.0
+        radarChartView.yAxis.axisMinimum = 0
+        radarChartView.yAxis.drawLabelsEnabled = true
+        
+        radarChartView.rotationEnabled = false
+        chartDataSet.drawFilledEnabled = true
+        
+        //Present the number as integer
+        let numberFormatter = NumberFormatter()
+        numberFormatter.generatesDecimalNumbers = false
+        chartDataSet.valueFormatter = numberFormatter as? IValueFormatter
+        radarChartView.yAxis.valueFormatter = numberFormatter as? IAxisValueFormatter
+        radarChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:months)
+        
+        //Other options
+        radarChartView.legend.enabled = false
+        radarChartView.yAxis.gridAntialiasEnabled = true
+        radarChartView.animate(yAxisDuration: 1.0)
+        
+        let chartData = RadarChartData(dataSet: chartDataSet)
+        chartData.labels = dataPoints
+        radarChartView.data = chartData
     }
-    */
-
+    
+    
+    //Funcion de la barChart
+    func setBarChart(dataEntryX forX:[String],dataEntryY forY: [Double]) {
+        barChart.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries:[BarChartDataEntry] = []
+        for i in 0..<forX.count{
+            let dataEntry = BarChartDataEntry(x: Double(i), y: Double(forY[i]) , data: months as AnyObject?)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChart.data = chartData
+        let xAxisValue = barChart.xAxis
+        xAxisValue.valueFormatter = axisFormatDelegate
+    }
+    
+    
 }
--- GS Sales SIM/ViewController.swift
+++ GS Sales SIM/ViewController.swift
@@ -9,48 +9,20 @@
 import UIKit
 import AVKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController: UIViewController {
     
-    let nombreEmpresa = ["Apple","Microsoft","Facebook","Ksquare"]
-
-    @IBOutlet weak var labelNombre: UITextField!
-    
-    var pickerNombre = UIPickerView()
-    var valorSeleccionado = false
+    @IBOutlet weak var rfc: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        pickerNombre.delegate = self
-        pickerNombre.dataSource = self
-        
-        labelNombre.inputView = pickerNombre
-    }
-    
-    //FUNCIONES DEL PICKER
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return nombreEmpresa.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return nombreEmpresa[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        valorSeleccionado = true
-        labelNombre.text = nombreEmpresa[row]
-        labelNombre.resignFirstResponder()
     }
     
     //VALIDACION DE SELECCION DE NOMBRE
     @IBAction func Practica(_ sender: Any) {
         
         //valor para saber si selecciono una empresa o en caso contrario manda alerta
-        if (valorSeleccionado != false){
+        if (rfc.text == "" || rfc.text == nil){
             performSegue(withIdentifier: "primerCuadrante", sender: self)
         }
         else{
@@ -65,7 +37,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     @IBAction func Registro(_ sender: Any) {
         //valor para saber si selecciono una empresa o en caso contrario manda alerta
-        if (valorSeleccionado != false){
+        if (rfc.text == "" || rfc.text == nil){
             performSegue(withIdentifier: "primerCuadrante", sender: self)
         }
         else{
